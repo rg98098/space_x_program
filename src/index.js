90ddadf7f001b141f98ddc39d884815383b2c7b4
Rahul <rg98098@gmail.com>
2020-11-02 11:36:29 +0530

    Remove compose

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import {Provider} from "react-redux";
-import {createStore,compose,applyMiddleware} from "redux";
+import {createStore,applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import App from "./App";
 import "./index.css";
 import fetchReducer from './store/reducers/fetchReducer';
 
-const composeEnhancers = process.env.NODE_ENV ==="development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
-const store = createStore(fetchReducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(fetchReducer,applyMiddleware(thunk));
 
 const app = (
  <Provider store={store}>
